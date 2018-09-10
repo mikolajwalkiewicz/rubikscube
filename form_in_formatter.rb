e4b6f019d1675641484ed87ccf81b4e88ec93368
mikolajwalkiewicz <38083921+mikolajwalkiewicz@users.noreply.github.com>
2018-09-10 09:37:05 +0200

    create file

---
+++ form_in_formatter.rb
@@ -0,0 +1,17 @@
+print "What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+
+print "What's your last name? "
+last_name = gets.chomp
+last_name.capitalize!
+
+print "What city are you from? "
+city = gets.chomp
+city.capitalize!
+
+print "What state or province are you from? "
+state = gets.chomp
+state.upcase!
+
+puts "You name is #{first_name} and your last name is #{last_name} and rest of your data is: #{city} #{state}" 
